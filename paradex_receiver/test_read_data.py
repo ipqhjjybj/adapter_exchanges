3fd517e2befab35df4eaf73e1d4a1683a16751e5
ipqhjjybj <250657661@qq.com>
2025-12-11 12:36:53 +0800

    add code

--- paradex_receiver/test_read_data.py
+++ paradex_receiver/test_read_data.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-data = pd.read_csv("/Users/shenzhuoheng/quant_yz/git/adapter_exchanges/data/paradex_book_snapshot_15_PAXG-USD-PERP_2025-12-11.csv")
-print(data)
+# data = pd.read_csv("/Users/shenzhuoheng/quant_yz/git/adapter_exchanges/data/paradex_book_snapshot_15_PAXG-USD-PERP_2025-12-11.csv")
+# print(data)
 
+data = pd.read_csv("/Users/shenzhuoheng/quant_yz/git/adapter_exchanges/data/paradex_trades_PAXG-USD-PERP_2025-12-11.csv")
+print(data)
 pass
